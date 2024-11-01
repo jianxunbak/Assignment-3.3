00d613431cff40a806027fbf8936811936d390eb
jianxun <jianxunbak@gmail.com>
2024-11-01 18:37:00 +0800

    completed assignment 3.3

--- assignment-code/LoopAssignment.java
+++ assignment-code/LoopAssignment.java
@@ -1,23 +1,58 @@
 public class LoopAssignment {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         // Task 1. Create a while loop that prints all the odd numbers from 1 to 50
-        
-        // Add code here
+        System.err.println("\nTask 1\n");
 
-        /* 
-            Task 2. Create a do-while loop that prints the following pattern from 1 to 30:
-            If the number is divisible by 3, print "Fizz"
-            If the number is divisible by 5, print "Buzz"
-            If the number is divisible by 3 AND 5, print "FizzBuzz"
-            If the number is not divisible by 3 nor 5, print "Pop"
-        */
+        boolean printing = true;
+        while (printing) {
+            for (int i = 1; i <= 50; i++) {
+                if (i % 2 != 0) {
+                    System.out.print(i + " ");
+                }
+                printing = false;
+            }
+        }
 
-        // Add code here
+        /*
+         * Task 2. Create a do-while loop that prints the following pattern from 1 to
+         * 30:
+         * If the number is divisible by 3, print "Fizz"
+         * If the number is divisible by 5, print "Buzz"
+         * If the number is divisible by 3 AND 5, print "FizzBuzz"
+         * If the number is not divisible by 3 nor 5, print "Pop"
+         */
 
-        // Task 3. Use an enhanced for-loop to compute for the average of the given array
+        System.err.println("\nTask 2\n");
+        boolean running = true;
+        do {
+            for (int i = 1; i <= 30; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println(i + ": " + "FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println(i + ": " + "Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println(i + ": " + "Buzz");
+                } else
+                    System.out.println(i + ": " + "Pop");
+            }
+            running = false;
 
-        int expenses[] = {120, 130, 114, 112, 132, 154, 123, 112, 165, 144, 188, 200};
-        float average = 0.0f;
-        // Add code here
+        } while (running);
+
+        // Task 3. Use an enhanced for-loop to compute for the average of the given
+        // array
+        System.err.println("\nTask 3\n");
+
+        int expenses[] = { 120, 130, 114, 112, 132, 154, 123, 112, 165, 144, 188, 200
+        };
+        float total = 0.0f;
+
+        for (int num : expenses) {
+            total += num;
+        }
+
+        float average = (total / expenses.length);
+        System.out.println("Average: " + average);
+
     }
 }
--- studies.md
+++
@@ -1,8 +0,0 @@
-## Self Studies
-
-- [Arrays in Java](https://www.geeksforgeeks.org/arrays-in-java/)
-- [Arrays in Java: A Reference Guide](https://www.baeldung.com/java-arrays-guide)
-- [ArrayList in Java](https://www.geeksforgeeks.org/arraylist-in-java/)
-- [Changing Arrays to ArrayList](https://www.tutorialspoint.com/Conversion-of-Array-To-ArrayList-in-Java)
-- [Java Loops](https://www.baeldung.com/java-loops)
-- [Loops in Java](https://www.geeksforgeeks.org/loops-in-java/)
